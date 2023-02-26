0fdb7df97782c8aba28b10550abec1580bfe46a5
Retroman95 <46816641+Retroman95@users.noreply.github.com>
2023-02-26 20:45:11 +0400

    Update task 1.sql

--- task 1.sql
+++ task 1.sql
@@ -1,21 +1,40 @@
-create table if not exists genre (
+create table if not exists artist_list(
 	id serial primary key,
-	genre_name varchar(100) not null unique
-);
-create table if not exists executor (
+	Name varchar(255) not null,
+	Alias varchar(80) not null);
+	
+create table if not exists artist_in_genre(
 	id serial primary key,
-	executor_name varchar(100) not null,
-	genre_id integer references genre(id)
-);
-create table if not exists album (
+	artist_id integer not null references artist_list(id),
+	genre_id integer not null references Genre_List(id));
+	
+create table if not exists Genre_list(
+	id serial primary key, 
+	Genre_Name  varchar(80) not null,
+							
+create table if not exists artist_album(
+	id  serial primary key,
+	artist_id integer not null references artist_list(id),
+	album_id integer not null references album_List(id));
+	
+create table if not exists album_List (
 	id serial primary key,
-	album_name varchar(100) not null,
-	year_of_release integer not null,
-	executor_id integer references executor(id)
-);
-create table if not exists track (
+	album_name varchar(80) not null,
+	release_date varchar(80) not null);
+	
+create table if not exists Track_List (
 	id serial primary key,
-	track_name varchar(100) not null unique,
-	duration integer not null,
-	album_id integer references album(id)
-);
+	album_id integer references album_List(id)
+	track_name varchar(80) not null,
+	track_time varchar(80) not null);
+		
+create table if not exists collection_List(
+	id serial primary key,
+	collection_name varchar(60) not null,
+	release_date  not null);
+							
+create table if not exists collection_album_track(
+	id serial primary key,
+	collection_id integer not null references collection_List(id),
+	album_id integer not null references album_List(id);
+	track_id integer not null references Track_List(id));
