bcaa9f11e49531b506696d43f378a91d9cedd61b
Retroman95 <46816641+Retroman95@users.noreply.github.com>
2023-03-04 21:12:18 +0400

    Update database_select.sql

--- database_select.sql
+++ database_select.sql
@@ -1,18 +1,22 @@
-SELECT year, name FROM album
-       WHERE year = 2018;
+SELECT  name, releasedate FROM album
+WHERE releasedate BETWEEN '2018-01-01' AND '2018-12-31';
 
-SELECT length, name FROM track
-       ORDER BY length DESC
-	   LIMIT 1;
 
-SELECT length, name FROM track
-       WHERE length >= 210;
+SELECT   name, tracklength FROM track
+ORDER BY tracklength DESC;
 
-SELECT year, name FROM collection
-       WHERE year BETWEEN 2018 AND 2020;
+
+SELECT  name FROM track
+WHERE tracklength >= 03.50;
+
+
+SELECT name FROM collections
+WHERE release_year BETWEEN '2018-01-01' AND '2020-12-31';
+
 
 SELECT name FROM artist
-       WHERE name NOT LIKE '%% %%';
+WHERE name NOT LIKE '%% %%';
+
 
 SELECT name FROM track
-       WHERE name LIKE '%%My%%';
+WHERE name LIKE '%%my%%';
